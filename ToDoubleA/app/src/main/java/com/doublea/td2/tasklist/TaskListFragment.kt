d4ec45d6d2b75c686a87655dd5b05736ea0d696f
Mimyke <mr_bo_yeux_@hotmail.fr>
2020-11-30 14:32:09 +0100

    ajout bouton

--- ToDoubleA/app/src/main/java/com/doublea/td2/tasklist/TaskListFragment.kt
+++ ToDoubleA/app/src/main/java/com/doublea/td2/tasklist/TaskListFragment.kt
@@ -8,9 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.doublea.td2.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class TaskListFragment : Fragment() {
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
             Task(id = "id_1", title = "Task 1", description = "description 1"),
             Task(id = "id_2", title = "Task 2"),
             Task(id = "id_3", title = "Task 3")
@@ -30,6 +32,12 @@ class TaskListFragment : Fragment() {
         var recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            // Instanciation d'un objet task avec des données préremplies:
+            taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
+            recyclerView?.adapter?.notifyDataSetChanged()
+        }
+
         // En utilisant les synthetics, on écrit juste l'id directement (c'est magique ✨):
         //recycler_view.layoutManager = LinearLayoutManager(activity)
     }
